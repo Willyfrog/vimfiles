a32a17b6ae90a40fc0e330c77e97da4a3ec092b2
Guillermo Vaya <guivaya@gmail.com>
2014-01-14 01:08:07 +0100

    retocando la config

--- qtile/config.py
+++ qtile/config.py
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
-# Note that since qtile configs are just python scripts, you can check for
-# syntax and runtime errors by just running this file as is from the command
-# line, e.g.:
-#
-#    python config.py
-
 from libqtile.config import Key, Screen, Group, Drag, Click, Match
 from libqtile.command import lazy
 from libqtile import layout, bar, widget, hook
+from libqtile.layout import floating
 from subprocess import Popen, check_output, PIPE, STDOUT
 from time import sleep
 from os import path
 import re
 
 
-# Theme
-#
+################################### SCREENS ####################################
+
 class Theme(object):
     bar = dict(
          opacity=0.9,
@@ -30,7 +25,7 @@ class Theme(object):
         )
 
     class Widget(object):
-        icons_path = '/user/share/icons/Faenza-Dark/status/24'
+        icons_path = '/user/share/icons/default'
         default = dict(
                 padding=1,
                 fontsize=14,
@@ -45,14 +40,14 @@ class Theme(object):
                 ).items())
         window_name = dict(default.items() + dict(
                 ).items())
-        # backlight = dict(default.items() + dict(
-        #         backlight_name='intel_backlight'
-        #         ).items())
+        backlight = dict(default.items() + dict(
+                 backlight_name='intel_backlight'
+                 ).items())
         volume = dict(default.items() + dict(
                 theme_path=icons_path
                 ).items())
         battery_icon = dict(default.items() + dict(
-                theme_path=icons_path,
+                #theme_path=icons_path,
                 battery_name='BAT1'
                 ).items())
         battery = dict(default.items() + dict(
@@ -69,7 +64,7 @@ class Theme(object):
         #        location='Madrid, SP'
         #        ).items())
         clock = dict(default.items() + dict(
-                fmt='%d.%m.%Y // %H:%M',  #'%a, %d de %b, %H:%M',
+                fmt='%d.%m.%Y | %H:%M',  #'%a, %d de %b, %H:%M',
                 fontsize=14,
                 padding=10
                 ).items())
@@ -79,8 +74,8 @@ class Theme(object):
                 ).items())
 
 
-# Screens
-#
+################################### SCREENS ####################################
+
 screens = [
     Screen(
         top = bar.Bar([
@@ -88,202 +83,149 @@ screens = [
             widget.Sep(padding=10),
             widget.CurrentLayout(**Theme.Widget.current_layout),
             widget.Sep(padding=10),
-            widget.WindowName(**Theme.Widget.window_name),
             widget.Prompt(**Theme.Widget.default),
+            widget.WindowName(**Theme.Widget.window_name),
+            #       widget.Mpris(),
             #widget.Backlight(**Theme.Widget.backlight),
             widget.Volume(**Theme.Widget.volume),
+            widget.Sep(padding=10),
             widget.BatteryIcon(**Theme.Widget.battery_icon),
             widget.Battery(**Theme.Widget.battery),
+            widget.Sep(padding=10),
             widget.Systray(),
             widget.Clock(**Theme.Widget.clock),
-            #widget.YahooWeather(**Theme.Widget.yahoo_weather),
-            #widget.Notify(foreground='#ffff00')
-        ], 22, **Theme.bar)
-    ),
-    Screen(
-        top = bar.Bar([
+            widget.Notify(foreground='#ffff00')
+        ], 22, **Theme.bar)),
+    Screen(top = bar.Bar(
+        [
             widget.GroupBox(**Theme.Widget.group_box),
             widget.Sep(padding=10),
             widget.CurrentLayout(**Theme.Widget.current_layout),
-            widget.Sep(padding=10),
             widget.WindowName(**Theme.Widget.window_name),
+            widget.Clock(**Theme.Widget.clock),
         ], 22, **Theme.bar)
-    ),
+       ),
 ]
 
+############################# KEYBOARD BINDINGS ################################
 
-# Key bindings
-#
 mod = "mod4"
 alt = "mod1"
 shift = "shift"
 ctrl = "control"
+
+TERMINAL = "urxvt"
+EDITOR = "emacsclient -ca \"\""
+BROWSER = "google-chrome-stable"
+
 keys = [
-    Key([shift, alt], "q",       lazy.shutdown()),
 
-    #Key([mod, ctrl], "j",        lazy.layout.section_down()),  # for tree layout
-    #Key([mod, ctrl], "k",        lazy.layout.section_up()),  # for tree layout
-    #Key([mod, ctrl], "h",        lazy.layout.collapse_branch()),  # for tree layout
-    #Key([mod, ctrl], "l",        lazy.layout.expand_branch()),  # for tree layout
+    Key([shift, alt], "q", lazy.shutdown()),
+    Key([mod,shift], "r", lazy.restart()),
+    Key([mod,shift], "x", lazy.window.kill()),
 
-    Key([mod, ctrl], "k",
-        lazy.layout.grow(),              # xmonad-tall
-        lazy.layout.decrease_nmaster()), # Tile
-    Key([mod, ctrl], "j",
-        lazy.layout.shrink(),            # xmonad-tall
-        lazy.layout.increase_nmaster()), # Tile
-    Key([mod, ctrl], "l",
-        lazy.layout.delete(),            # Stack
-        lazy.layout.increase_ratio(),    # Tile
-        lazy.layout.grow()),             # xmonad-tall
-    Key([mod, ctrl], "h",
-        lazy.layout.add(),               # Stack
-        lazy.layout.decrease_ratio(),    # Tile
-        lazy.layout.shrink()),           # xmonad-tall
+    Key([mod], "r", lazy.spawncmd()),
+    Key([mod], "g", lazy.switchgroup()),
 
-    Key([mod], "k",              lazy.layout.down()),
-    Key([mod], "j",              lazy.layout.up()),
-    Key([mod], "h",              lazy.layout.previous()),
-    Key([mod], "l",              lazy.layout.next()),
+    Key([mod], "Return", lazy.spawn(TERMINAL)),
+    Key([mod,shift], "Return", lazy.spawn(EDITOR)),
 
-    Key([mod, alt], "h",         lazy.to_screen(0)),
-    Key([mod, alt], "l",         lazy.to_screen(1)),
+    Key([mod], "k", lazy.layout.up()),
+    Key([mod], "j", lazy.layout.down()),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),  # Stack, xmonad-tall
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),  # Stack, xmonad-tall
+    Key([mod], "Tab", lazy.layout.next()),
+    Key([mod,shift], "Tab", lazy.layout.previous()),
+    Key([mod], "h", lazy.layout.previous()),
+    Key([mod], "l", lazy.layout.next()),
+    Key([mod, shift], "f", lazy.window.toggle_floating()),
+    Key([mod, shift], "m", lazy.window.toggle_maximized()),
 
-    Key([ctrl, alt], "Up",       lazy.to_screen(0), lazy.screen.prevgroup()),
-    Key([ctrl, alt], "Down",     lazy.to_screen(0), lazy.screen.nextgroup()),
-    Key([ctrl, alt, shift], "Up", lazy.to_screen(1), lazy.screen.prevgroup(), lazy.to_screen(0)),
-    Key([ctrl, alt, shift], "Down", lazy.to_screen(1), lazy.screen.nextgroup(), lazy.to_screen(0)),
+    Key([mod], "space", lazy.nextlayout()),
+    Key([mod,shift], "space", lazy.prevlayout()),
 
-    Key([mod, "shift"], "k",
-        lazy.layout.shuffle_up()),       # Stack, xmonad-tall
-    Key([mod, "shift"], "j",
-        lazy.layout.shuffle_down()),     # Stack, xmonad-tall
+    Key([mod], "Left", lazy.group.prevgroup()),
+    Key([mod], "Right", lazy.group.nextgroup()),
 
-    Key([mod, shift], "space",
-        lazy.layout.rotate(),
-        lazy.layout.flip()),             # xmonad-tall
+    Key([mod,shift], "Left", lazy.to_next_screen()),
+    Key([mod,shift], "Right", lazy.to_prev_screen()),
 
-    Key([alt, shift], "Tab",     lazy.layout.previous()),
-    Key([alt], "Tab",            lazy.layout.next()),
 
-    Key([mod, shift], "Return",  lazy.layout.toggle_split()),
-    Key([mod, shift], "f",       lazy.window.toggle_floating()),
-
-    Key([alt], "space",          lazy.nextlayout()),
-    Key([alt, shift], "space",   lazy.prevlayout()),
-
-    Key([mod], "x",              lazy.window.kill()),
-    Key([mod, ctrl], "r",        lazy.restart()),
-
-    # interact with prompts
-    Key([mod], "r",              lazy.spawncmd()),
-    Key([mod], "g",              lazy.switchgroup()),
+    Key([mod, shift], "space", lazy.layout.rotate()),
+    Key([mod, shift], "minus", lazy.layout.toggle_split()),
 
     # start specific apps
-    Key([alt], "F2",
-            lazy.spawn("dmenu_run -p run -fn 'Terminus-13' -nb '#202020' -nf '#ffffff'")),
-    Key([mod], "Return",         lazy.spawn("urxvt")),
-    Key([mod, alt], "Return",    lazy.group["code"].toscreen()),
-    Key([mod], "b",              
-            lazy.spawn("chromium-browser"),
-            lazy.group["www"].toscreen()),
-    Key([mod, alt], 'b',         lazy.group["www"].toscreen()),
+#    Key([mod], "F1",              lazy.spawn("google-chrome")),
+#    Key([mod], "F2",              lazy.spawn("thunderbird")),
+#    Key([mod], "F3",              lazy.spawn("spotify")),
+#    Key([mod], "F4",              lazy.spawn("psi")),
 
-    # Multimedia keys
-    Key([], "XF86AudioRaiseVolume",  lazy.spawn("amixer -c 0 -q set Master 2dB+")),
-    Key([], "XF86AudioLowerVolume",  lazy.spawn("amixer -c 0 -q set Master 2dB-")),
-    Key([], "XF86AudioMute",         lazy.spawn("amixer -D pulse -q set Master toggle")),
-    Key([], "XF86MonBrightnessDown", lazy.spawn("xbacklight -dec 10")),
-    Key([], "XF86MonBrightnessUp",   lazy.spawn("xbacklight -inc 10")),
-    #Key([], "XF86TouchpadToggle",   lazy.spawn("synclient TouchpadOff=$(synclient -l | grep -c 'TouchpadOff.*=.*0')")),
+    # Change the volume if your keyboard has special volume keys.
+    Key([], "XF86AudioRaiseVolume",
+        lazy.spawn("amixer -c 0 -q set Master 2dB+")),
+    Key([], "XF86AudioLowerVolume",
+        lazy.spawn("amixer -c 0 -q set Master 2dB-")),
+    Key([], "XF86AudioMute",
+        lazy.spawn("amixer -c 0 -q set Master toggle")),
     Key([], 'XF86Calculator',        lazy.spawn('xcalc')),
-    Key([], 'XF86Mail',              lazy.spawn('chromium-browser "http://mail.google.com"'), lazy.group["www"].toscreen()),
-    Key([], 'XF86HomePage',          lazy.to_screen(0), lazy.group["code"].toscreen()),
-
-    # lock screen
-    Key([ctrl, alt], "l", lazy.spawn("screenlock")),
+    Key([mod], "XF86Calculator", 
+        lazy.spawn("xscreensaver-command -lock")),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("xbacklight -dec 10")),
+    Key([], "XF86MonBrightnessUp", lazy.spawn("xbacklight -inc 10")),
+    #Key([], "XF86TouchpadToggle",   lazy.spawn("synclient TouchpadOff=$(synclient -l | grep -c 'TouchpadOff.*=.*0')")),
+    Key([], 'XF86Mail', lazy.spawn('%s "http://mail.google.com"' % BROWSER)),
+    #Key([], 'XF86HomePage', lazy.to_screen(0), lazy.group["code"].toscreen()),
 ]
 
+############################## MOUSE BINDINGS ##################################
 
-# Mouse
-#
 mouse = [
-    Drag([mod], "Button1",
-        lazy.window.set_position_floating(),
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
         start=lazy.window.get_position()),
-    Drag([mod], "Button3",
-        lazy.window.set_size_floating(),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
         start=lazy.window.get_size()),
-    Click([mod], "Button2",
-        lazy.window.bring_to_front())
+    Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
+##################################  GROUPS  ####################################
 
-# Groups
-#
-groups = []
-# groups.extend([
-#     Group('www', layout='max',
-#         matches=[Match(wm_class=["chromium", "Chromium", "Navigator", "Iceweasel"])]),
-#     Group('code', layout='max'),
-# ])
-for i in ["1", "2", "3", "4", "5"]:
-    groups.append(Group(i, layout='max'))
-# groups.extend([
-#     #Group('gvim', layout='max', persist=False, init=False, screen_affinity=1, matches=[Match(wm_class=["gvim", "Gvim"])]),
-#     Group('music', layout='max', persist=False, init=False),
-#     Group('gimp', layout='gimp', persist=False, init=False,
-#         matches=[Match(wm_class=['gimp', 'Gimp'])]),
-#     Group('screen2', layout='max'),
-# ])
-for i, group in enumerate(groups):
-    keys.extend([
-        Key([mod], str(i + 1), lazy.group[group.name].toscreen()),
-        Key([mod, shift], str(i + 1), lazy.window.togroup(group.name))
-    ])
+groups = [
+    Group("1"),
+    Group("2"),
+    Group("3"),
+    Group("4"),
+    Group("5"),
+    Group("6"),
+    Group("7"),
+    Group("8"),
+]
+for i in groups:
+    # mod1 + letter of group = switch to group
+    keys.append(Key([mod], i.name, lazy.group[i.name].toscreen()))
+
+    # mod1 + shift + letter of group = switch to & move focused window to group
+    keys.append(Key([mod, shift], i.name, lazy.window.togroup(i.name)))
 
 
-# Layouts
-#
+##################################  LAYOUTS  ###################################
+
 layouts = [
-    layout.Max(),
-    #layout.TreeTab(sections=['Surfing', 'E-mail', 'Docs', 'Incognito']),
+    layout.MonadTall(**Theme.Layout.border_args),
+    layout.Tile(**Theme.Layout.border_args),
     layout.Stack(stacks=2, **Theme.Layout.border_args),
-    layout.MonadTall(),
-    layout.Tile(),
-    #layout.Zoomy(),
-
-    # a layout just for gimp
-    layout.Slice('left', 192, name='gimp', role='gimp-toolbox',
-         fallback=layout.Slice('right', 256, role='gimp-dock',
-         fallback=layout.Stack(stacks=1, **Theme.Layout.border_args))),
+    layout.Max(),
+    layout.TreeTab(),  # display tree windows on left
+#   layout.RatioTile(),
 ]
 
+# setting default auto flot types and float rules
+floating_layout = layout.Floating(auto_float_types = floating.DEFAULT_FLOAT_WM_TYPES,
+    float_rules = [dict(wmclass="spotify"),dict(wmclass="gnome-calculator"),dict(wname="Unlock Login Keyring")])
 
-# Hooks
-#
-def is_running(process):
-    s = Popen(["ps", "axuw"], stdout=PIPE)
-    for x in s.stdout:
-        if re.search(process, x):
-            return True
-    return False
-
-
-def execute_once(process):
-    if not is_running(process):
-        return Popen(process.split())
-
-
-def cmd_get(process):
-    return check_output("%s; exit 0;" % process, stderr=STDOUT, shell=True)
-
-
+# make dialog floating
 @hook.subscribe.client_new
-def dialogs(window):
-    if (window.window.get_wm_type() == 'dialog'
-            or window.window.get_wm_transient_for()
-            ):
+def floating_dialogs(window):
+    if(window.window.get_wm_type() == 'dialog' or window.window.get_wm_transient_for()):
         window.floating = True
 
     try:
@@ -306,6 +248,21 @@ def dialogs(window):
             logfile.write(''.join(['\n', str(e)]))
             return
 
+def is_running(process):
+    s = Popen(["ps", "axuw"], stdout=PIPE)
+    for x in s.stdout:
+        if re.search(process, x):
+            return True
+    return False
+
+
+def execute_once(process):
+    if not is_running(process):
+        return Popen(process.split())
+
+
+def cmd_get(process):
+    return check_output("%s; exit 0;" % process, stderr=STDOUT, shell=True)
 
 def setup_screens():
     num_screens = int(cmd_get("xrandr|grep -c \ connected"))
